62b2dd37ce27b9a0c666971dd5fd0fa502163cb6
Saqib Saleem <24PWBCS1269@uetpeshawar.edu.pk>
2026-05-02 00:26:13 +0500

    Adding calculator logic for real

--- hello.js
+++ hello.js
@@ -1,3 +1,22 @@
 console.log("Hello, World!");
 console.log("Start");
 console.log('End');
+
+
+// Simple logic Calculator
+
+let num1 = 10;
+let num2 = 5;
+
+let sum = num1 + num2;
+let sub = num1 - num2;
+let mul = num1 * num2;
+let div = num1 / num2;
+
+console.log("Number 1:", num1);
+console.log("Number 2:", num2);
+
+console.log("Addition:", sum);
+console.log("Subtraction:", sub);
+console.log("Multiplication:", mul);
+console.log("Division:", div);
