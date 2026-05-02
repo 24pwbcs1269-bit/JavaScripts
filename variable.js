53f12bd890a48e45d710262ebc594bd8adb1f68e
Saqib Saleem <24PWBCS1269@uetpeshawar.edu.pk>
2026-05-02 23:32:46 +0500

    learned a Variable of Javascripts

---
+++ variable.js
@@ -0,0 +1,46 @@
+age = 21 
+name = "Saqib Saleem"
+Name = "Shahzeb Ahmed"
+radius= 14
+isfollowing = false
+
+console.log("name: " + name)
+console.log("Name with capital N: " + Name)
+console.log("Age: " + age)
+console.log("Radius: " + radius)
+console.log("Is following: " + isfollowing)
+
+age = age + 1
+console.log("Age after one year: " + age)
+
+radius = radius * 2
+console.log("Radius after doubling: " + radius)
+
+
+
+let brands = ["Apple", "Samsung", "OnePlus", "Google", "Sony"]
+let price = [999, 899, 699, 799, 499]
+const Pi = 3.14
+
+console.log("Brands: " + brands)
+console.log("Prices: " + price)
+console.log("Value of Pi: " + Pi)
+
+brands.push("Huawei")
+price.push(399)
+
+console.log("Updated Brands: " + brands)
+console.log("Updated Prices: " + price)
+
+
+const product ={
+    name: "Pen",
+    rating: 4.5,
+    price: 1.99,
+    inStock: true
+}
+console.log(product)
+
+
+
+
